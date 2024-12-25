277d9c91877fc545dc6b3c11db309ab9e818594c
PSG4160 <psg4160@gmail.com>
2024-12-25 18:03:26 +0900

    add/ feature products/models

--- spartamarket_DRF/products/models.py
+++ spartamarket_DRF/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+# 상품 이미지를 저장할 경로를 생성하는 함수
+def product_image_path(instance, filename):
+    return f'product_images/{instance.user.username}/{filename}'
+
+# 사용자의 등록한 상품을 저장하는 모델
+class Product(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="products")
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to=product_image_path, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
